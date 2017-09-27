ca316b20895da50ca897522b76857fcb8973d8db
azraelzc <460366051@qq.com>
2017-09-27 17:57:22 +0800

    bat字符串读取
    读取失败先提交回去搞

--- Server/initDB.bat
+++ Server/initDB.bat
@@ -1,16 +1,20 @@
 @SET DBPath="C:\Program Files\MySQL\MySQL Server 5.5\bin"
 @SET PWD=123456
 @SET DBName=zcgame
-cd /d %DBPath%
-echo "11111111"
-mysql -u root -p%PWD% %DBName% --execute=
-"
-	drop table test if exists;
-	create table test(
-	id int(10) unsigned NOT NULL AUTO_INCREMENT,
-	name varchar(10) NOT NULL DEFAULT '',
-);
-"
+@SET SolutionDir=%~dp0
+@SET SQLFile=%SolutionDir%sql.txt
+@SET SQLStr = ""
+echo %SQLFile%
+set aa=伟大的中国！
+set bb=我为你自豪
+echo %aa%%bb%
 
---execute=""
+echo aa=%aa%
+echo bb=%bb%
+set "aa=%aa%%bb%"
+echo aa=%aa%
+
+FOR /F "delims=" %%i in (%SQLFile%) do (SET SQLStr=%SQLStr%%PWD%)
+echo %SQLStr%
+cd /d %DBPath%
 @pause
